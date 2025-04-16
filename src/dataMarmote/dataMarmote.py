4cdd78b59cc152c6031b227b47dfc7a2ba5649f3
ZEYU TAO <72023720+bellkilo@users.noreply.github.com>
2025-04-16 21:39:35 +0200

    dataMarmote with writter version 0.0.1

--- src/dataMarmote/dataMarmote.py
+++ src/dataMarmote/dataMarmote.py
@@ -1,3 +1,5 @@
+import json
+import os
 from typing import Optional, Dict, List, Set, Union
 
 from marmote.core import MarmoteBox, FullMatrix, SparseMatrix, MarmoteInterval, DISCRETE
@@ -76,7 +78,7 @@ class DataMarmote:
                         for destination, value in destinations:
                             i = stateSpace.Index(state_out)
                             j = stateSpace.Index(destination)
-                            trans[index].setEntry(i,j,value)
+                            trans[index].setEntry(i,j,value[0])
             else:
                 trans = self.transitions
 
@@ -96,8 +98,8 @@ class DataMarmote:
                 raise ValueError(f"Invalid type: {chosen_type}.")
         else:
             P = SparseMatrix(stateSpace)
-            # to do not sure
-            transitions = self.transitions[0]
+            # to do, not sure
+            transitions = self.transitions
             for state_out, destinations in transitions.items():
                 for destination, value in destinations:
                     i = stateSpace.Index(state_out)
@@ -105,3 +107,127 @@ class DataMarmote:
                     P.setEntry(i,j,value)
             P.set_type(DISCRETE)
             return MarkovChain(P)
+
+    @staticmethod
+    def __build_expression(variables, values):
+        def recurse(items):
+            if len(items) == 1:
+                var, val = items[0]
+                return {"op": "=", "left": var, "right": int(val)}
+            else:
+                mid = len(items) // 2
+                left_expr = recurse(items[:mid])
+                right_expr = recurse(items[mid:])
+                return {
+                    "op": "∧",
+                    "left": left_expr,
+                    "right": right_expr
+                }
+
+        variable_value_pairs = list(zip(variables, values))
+        expression = {
+            "exp": recurse(variable_value_pairs)
+        }
+        return expression
+
+    def __create_jani_model(self):
+        stateSpace = self.stateSpace
+        actionSpace = self.actionSpace
+        if actionSpace is not None:
+            num_actions = len(actionSpace)
+        else:
+            num_actions = 1
+
+        dims = stateSpace.tot_nb_dims()
+        variable_names = [f"x{i + 1}" for i in range(dims)]
+        variables = []
+
+        for i, name in enumerate(variable_names):
+            variable_dict = {
+                "name": name,
+                "type": {
+                    "kind": "bounded",
+                    "base": "int",
+                    "lower-bound": 0,
+                    "upper-bound": stateSpace.CardinalbyDim(i) - 1
+                },
+            }
+            variables.append(variable_dict)
+
+        model = {
+            "jani-version": 1,
+            "name": self.name,
+            "type": self.type,
+            "criterion": self.criterion,
+            "features": ["rewards"],
+            "variables": variables,
+            "actions": [
+                {"name": f"action{i}"} for i in range(num_actions)
+            ],
+            "automata": [{
+                "name": "automata0",
+                "locations": [{"name": "loc0"}],
+                "initial-locations": ["loc0"],
+                "edges": []
+            }],
+            "system": {
+                "elements": [
+                    {
+                        "automaton": "automata0"
+                    }]
+            }
+        }
+
+        automaton = model["automata"][0]
+        if isinstance(self.transitions, dict):
+            transitions = self.transitions
+            actions = list(self.actionSpace)
+            actions.sort()
+            for index, action in enumerate(actions):
+                transition = transitions[action]
+                for state_out, destinations in transition.items():
+                    destinations = []
+                    for destination, value in destinations:
+                        prob, reward = value
+                        destinations.append({
+                            "location": "loc0",
+                            "probability": {
+                                "exp": prob
+                            },
+                            "assignments": [
+                                {
+                                    "ref": variable_names[n],
+                                    "value": int(destination[n])
+                                } for n in range(dims)
+                            ],
+                            "rewards": {
+                                "exp": reward
+                            }
+                        })
+                    automaton["edges"].append({
+                        "location": "loc0",
+                        "action": action,
+                        "guard":
+                            self.__build_expression(variable_names, state_out)
+                        ,
+                        "destinations": destinations
+                    })
+        return model
+
+
+    def save(self, filename: str):
+        filename = f"{filename}.janiR"
+        counter = 1
+        while os.path.exists(filename):
+            filename = f"{filename.split('.')[0]}_{counter}.janiR"
+            counter += 1
+
+        model = self.__create_jani_model()
+        jani_content = json.dumps(model, indent=2)
+
+        with open(filename, 'w') as file:
+            file.write(jani_content)
+        print(f"Model saved as {filename}")
+
+        #  action        i       dest    prob  reward
+        # {"action1" : {(0,0): {(0,1) : (0.5, 100)}}}
