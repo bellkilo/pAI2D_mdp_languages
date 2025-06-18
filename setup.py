0cebe1fd05f9a6bb8802ea5c9112c58a777d74eb
JiahuaLiY <jiahua.li.y@gmail.com>
2025-06-18 15:56:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="janiParser",
     version="0.1.0",
     author="Jiahua Li, Zeyu Tao",
-    description="A tool for parsinga subset of JANI models, " \
-    "solving them using Marmote library and performing benchmarking",
+    description="A tool for parsing a subset of JANI models, " \
+        "solving them primarily with the Marmote library, with optional support for MDPToolbox, " \
+        "and performing benchmarking.",
     url="https://github.com/bellkilo/pAI2D_mdp_languages",
     packages=find_packages(),
     install_requires=[
